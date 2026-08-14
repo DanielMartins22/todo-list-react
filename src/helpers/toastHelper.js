2d486406097c6ec65f1304b49178a25629dc8311
Developer <dev@example.com>
2026-08-14 12:13:19 -0300

    feat: adicionar utilitários de notificação (toastHelper)

---
+++ src/helpers/toastHelper.js
@@ -0,0 +1,10 @@
+import { toast } from "react-toastify";
+
+export const toastHelper = {
+    add: () => toast.success("Sua tarefa foi adicionada!"),
+    delete: () => toast.success("Tarefa deletada com sucesso!"),
+    editMode: () => toast.info("Modo editar acionado!"),
+    save: () => toast.info("Salvando sua tarefa...") ,
+    done: () => toast.success("Tarefa concluída com sucesso!") ,
+    undo: () => toast.info("Desfazendo sua escolha!"),
+}
