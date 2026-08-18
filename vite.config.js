9d544ba69303ae2636615e813e1a9235b320ed12
Developer <dev@example.com>
2026-08-18 12:41:52 -0300

    fix vite base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: 'https://danielmartins22.github.io/todo-list-react/'
+	base: '/todo-list-react/'
 });
